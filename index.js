dac435467b653ad1af5f263be1af7b22d24763ff
iamruinous / peterdemartini <jade.meskill@gmail.com , thepeterdemartini@gmail.com>
2017-06-02 14:48:34 -0700

    v11.0.4 try to use dist version of runner

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-'use strict';
-require('coffee-script/register');
-module.exports = require('./src/index');
+"use strict"
+
+var pathExists = require('path-exists');
+var index = null;
+
+if (pathExists.sync('./dist/index.js')) {
+  index = require('./dist/index')
+} else {
+  require('coffee-script/register');
+  index = require('./src/index');
+}
+
+module.exports = index
--- runner.js
+++ runner.js
@@ -1,2 +1,13 @@
 "use strict"
-module.exports = require("./src/runner")
+
+var pathExists = require('path-exists');
+var runner = null;
+
+if (pathExists.sync('./dist/runner.js')) {
+  runner = require('./dist/runner')
+} else {
+  require('coffee-script/register');
+  runner = require('./src/runner');
+}
+
+module.exports = runner
